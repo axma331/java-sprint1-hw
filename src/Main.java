4c40a8b008791474e8d30771bcc9f9a49799782b
Akhmed Ismailov <i7mailov@ya.ru>
2022-10-31 22:07:15 +0300

    create  printMenu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Пришло время практики!");
+        Scanner scanner = new Scanner(System.in);
+        printMenu();
+        int userInput = scanner.nextInt();
+
+        while (userInput != 0) {
+            // обработка разных случаев
+
+            printMenu(); // печатаем меню ещё раз перед завершением предыдущего действия
+            userInput = scanner.nextInt(); // повторное считывание данных от пользователя
+        }
+        System.out.println("Программа завершена");
+    }
+
+    private static void printMenu() {
+        System.out.println("Выберите действие, которое хотите выполнить:");
+        System.out.println("1. Ввести количество шагов за определённый день;");
+        System.out.println("2. Напечатать статистику за определённый месяц;");
+        System.out.println("3. Изменить цель по количеству шагов в день;");
+        System.out.println("4. Выйти из приложения.");
     }
 }
